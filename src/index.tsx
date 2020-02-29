36c8a041654f5f3cf539cce3bcc63af4765506db
Charles Oliver <charles@cloudThing.com>
2020-02-29 18:47:22 +0000

    combine App and index

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,63 @@
-import React from "react";
+import React, { useContext } from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
-import { App } from "./App";
-import { StoreProvider } from "./common/state/Store";
 import * as serviceWorker from "./serviceWorker";
+import { Answer, QuizState } from "./common/constants";
+import { StoreProvider } from "./common/state/Store";
+import { store } from "./common/state/Store";
+import { questions } from "./feature/quiz/quizData";
+import { Home } from "./feature/home/Home.view";
+import { Quiz } from "./feature/quiz/Quiz.view";
+import { Results } from "./feature/results/Results.view";
+// import { Settings } from "./feature/settings/Settings.view";
+
+const App: React.FC = () => {
+  const { state, dispatch } = useContext(store);
+
+  const { answers, quizState } = state;
+
+  const setQuizState = (quizState: QuizState) =>
+    dispatch(["SET_QUIZ_STATE", quizState]);
+
+  const currentQuestion = answers.length;
+  const lastQuestion = questions.length - 1;
+
+  const handleQuestionAnswered = (answer: Answer) => {
+    dispatch(["ANSWER_QUESTION", answer]);
+
+    if (currentQuestion === lastQuestion) {
+      setQuizState("finished");
+    }
+  };
+
+  return (
+    <div>
+      {quizState === "init" && (
+        <Home
+          onQuizStarted={() => setQuizState("started")}
+          questionCount={questions.length}
+        />
+      )}
+      {quizState === "started" && (
+        <Quiz
+          questions={questions}
+          currentQuestion={currentQuestion}
+          onQuestionAnswered={handleQuestionAnswered}
+        />
+      )}
+      {quizState === "finished" && (
+        <Results
+          questions={questions}
+          answers={answers}
+          onQuizRestarted={() => {
+            setQuizState("init");
+          }}
+        />
+      )}
+    </div>
+  );
+};
 
 ReactDOM.render(
   <StoreProvider>
