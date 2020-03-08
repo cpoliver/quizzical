cc2aeff58087a78fa582ea5c9d22b0c25a3203ae
Charles Oliver <charles@cloudThing.com>
2020-03-08 16:12:49 +0000

    wip: add progress ring

--- src/feature/quiz/QuestionSettings.tsx
+++ src/feature/quiz/QuestionSettings.tsx
@@ -23,16 +23,14 @@ export const QuestionSettings: React.FC = () => {
         >
           -
         </Button>
-        <Flex>
-          <Box
-            sx={{
-              flex: 1,
-              height: questionCount / MAX_QUESTION_COUNT,
-              backgroundColor: "orange",
-              width: 50,
-            }}
+        <Box flex={1}>
+          <Ring
+            count={questionCount}
+            total={MAX_QUESTION_COUNT}
+            stroke={2}
+            radius={120}
           />
-        </Flex>
+        </Box>
         <Button
           variant="answer"
           onClick={() => dispatch(["INCREASE_DIFFICULTY"])}
@@ -51,5 +49,36 @@ export const QuestionSettings: React.FC = () => {
         </Text>
       </Box>
     </Flex>
+  );
+};
+
+type RingProps = {
+  count: number;
+  total: number;
+  radius: number;
+  stroke?: number;
+};
+
+const Ring: React.FC<RingProps> = ({ count, total, radius, stroke = 2 }) => {
+  const percent = count / total;
+  const normalizedRadius = radius - stroke * 2;
+  const circumference = normalizedRadius * 2 * Math.PI;
+  const offset = circumference - percent * circumference;
+
+  return (
+    <svg height={radius * 2} width={radius * 2}>
+      <circle
+        stroke="white"
+        stroke-dasharray={`${circumference} ${circumference}`}
+        style={{
+          strokeDashoffset: offset,
+        }}
+        stroke-width={stroke}
+        fill="transparent"
+        r={normalizedRadius}
+        cx={radius}
+        cy={radius}
+      />
+    </svg>
   );
 };
