73f51f4c61a929cb71c5fdec273b596537ec2438
Charles Oliver <charles@cloudThing.com>
2020-03-14 15:52:35 +0000

    update theme

--- src/common/theme.ts
+++ src/common/theme.ts
@@ -1,11 +1,23 @@
-import { borderWidth } from "styled-system";
-
 // Theme UI spec: https://theme-ui.com/customize
 
 const DEFAULT_FONTS =
   "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', sans-serif";
 
 export const theme = {
+  breakpoints: ["40em", "52em", "64em"],
+  space: [0, 4, 8, 12, 16, 24, 32, 48, 64],
+
+  colors: {
+    primary: "#f3f9ff",
+    background: "#2c7ef8",
+    secondary: "#3b4660",
+    faded: "#92bbf8",
+    backgroundSecondary: "#f3f9ff",
+    grey: "#54565a",
+    correct: "#0e8729",
+    incorrect: "#bf0000",
+  },
+
   fonts: {
     body: `'Rubik', ${DEFAULT_FONTS}`,
     heading: `'Rubik', ${DEFAULT_FONTS}`,
@@ -20,17 +32,7 @@ export const theme = {
     semi: 500,
     bold: 700,
   },
-  space: [0, 4, 8, 12, 16, 24, 32, 48, 64],
-  colors: {
-    primary: "#f3f9ff",
-    background: "#2c7ef8",
-    secondary: "#3b4660",
-    faded: "#92bbf8",
-    backgroundSecondary: "#f3f9ff",
-    grey: "#54565a",
-    correct: "#0e8729",
-    incorrect: "#bf0000",
-  },
+
   buttons: {
     default: {
       backgroundColor: "primary",
