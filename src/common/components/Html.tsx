596d21116a3e2505fa24b9da4e8d891c1aace370
Charles Oliver <charles@cloudThing.com>
2020-04-10 11:48:13 +0100

    configure mdx support for storybook

--- src/common/components/Html.tsx
+++ src/common/components/Html.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
-export const Html: React.FC<{ html: string }> = ({ html }) => (
+type HtmlProps = { html: string };
+
+export const Html: React.FC<HtmlProps> = ({ html }: HtmlProps) => (
   <div dangerouslySetInnerHTML={{ __html: html }} />
 );
