61b3fb54a77110711b17f7c0398be17e9b129b8d
Charles Oliver <charles@cloudThing.com>
2020-04-12 12:14:25 +0100

    add stories for view components

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,14 +2,17 @@ import React from "react";
 import { Box } from "rebass";
 import { addDecorator } from "@storybook/react";
 import { ThemeProvider } from "theme-ui";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import { StoreProvider } from "../src/common/state/Store";
 import { theme } from "../src/common/theme";
 
 addDecorator(storyFn => (
-  <StoreProvider useMocks>
-    <ThemeProvider theme={theme}>
-      <Box p={5}>{storyFn()}</Box>
-    </ThemeProvider>
-  </StoreProvider>
+  <Router>
+    <StoreProvider useMocks>
+      <ThemeProvider theme={theme}>
+        <Box p={5}>{storyFn()}</Box>
+      </ThemeProvider>
+    </StoreProvider>
+  </Router>
 ));
