505b210b6f8ecf33e7263148f8655bad435daf5c
Charles Oliver <charles@cloudThing.com>
2020-04-15 09:58:26 +0100

    add gradient fade to theme colors

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="react-scripts" />
 
-// suppress ts issues for untyped module
+// suppress ts issues for untyped modules
 declare module "@rebass/preset";
+declare module "hex2rgb";
